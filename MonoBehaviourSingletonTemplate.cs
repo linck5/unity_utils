45a640c71bc84fd7d7939b5d2aa3873c1ea97fca
Linck5 <linck_192@hotmail.com>
2016-01-26 15:20:21 -0200

    Added MonoBehaviourSingletonTemplate

---
+++ MonoBehaviourSingletonTemplate.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonoBehaviourSingletonTemplate : MonoBehaviour {
+
+    private static MonoBehaviourSingletonTemplate instance = null;
+    public static MonoBehaviourSingletonTemplate inst {
+        get { if (!instance) instance = GameObject.FindObjectOfType<MonoBehaviourSingletonTemplate>(); return instance; }
+    }
+	
+}
